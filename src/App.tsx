91fbc68389af6371ef25fd56ed594e1f40c4d157
Nico <nicodann@gmail.com>
2022-09-02 21:42:01 -0400

    Add logic to render status messages from status prop with only 3 options - string literals

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
   const personName = {
@@ -29,6 +30,7 @@ function App() {
       <Greet name='Vishwas' messageCount={20} isLoggedIn={false}/>
       <Person name={personName}/>
       <PersonList names={nameList} />
+      <Status status='success'/>
     </div>
   );
 }
--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,9 +1,19 @@
-export const Status = () => {
+type StatusProps = {
+  status: 'loading' | 'success' | 'error'
+}
+
+export const Status = (props: StatusProps) => {
+  let message;
+  if(props.status === 'loading') {
+    message = 'Loading...'
+  } else if (props.status === 'success') {
+    message = 'Data fetched successfully!'
+  } else if (props.status === 'error') {
+    message = 'Error fetching data'
+  }
   return (
     <div>
-      <h2>Loading...</h2>
-      <h2>Data fetched successfully!</h2>
-      <h2>Error fetching data</h2>
+      <h2>{message}</h2>
     </div>
   )
 }
