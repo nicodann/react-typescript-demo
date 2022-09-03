f30487b5199000cbe5b67fa2fd3eac4b0185faa6
Nico <nicodann@gmail.com>
2022-09-02 21:31:31 -0400

    Make initial componenets and props.

---
+++ src/components/PersonList.tsx
@@ -0,0 +1,20 @@
+type PersonListProps = {
+  names: {
+    first: string,
+    last: string
+  }[]
+}
+
+export const PersonList = (props: PersonListProps) => {
+  return (
+    <div>
+      {props.names.map((name, index) => {
+        return (
+          <h2 key={index}>
+            {name.first} {name.last}
+          </h2>
+        )
+      })}
+    </div>
+  )
+}
