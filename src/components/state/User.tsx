840d62659ea85dcdcc5d5b1b836d9729fb307a73
Nico Dann <nicodann@gmail.com>
2022-11-28 11:13:59 -0500

    Add ? to allow for possible null when accessing value.

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -20,7 +20,8 @@ export const User = () => {
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      <div>User name is</div>
+      <div>User name is {user?.name}</div>
+      <div>User email is {user?.email}</div>
     </div>
   )
 }
