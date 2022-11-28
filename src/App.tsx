b5ab8f1a4a02215ed2b86779761a7c135735e056
Nico Dann <nicodann@gmail.com>
2022-11-28 10:04:59 -0500

    Add Button and Input components and their types.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { PersonList } from './components/PersonList';
 import { Status } from './components/Status';
 import { Heading } from './components/Heading';
 import { Oscar } from './components/Oscar';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   const personName = {
@@ -40,6 +42,12 @@ function App() {
         </Heading>
       </Oscar>
       <Greet  name='Vishwas' isLoggedIn={false}/>
+      <Button 
+        handleClick={(event, id) => {
+          console.log('Button clicked', event, id)
+        }}
+      />
+      <Input value='' handleChange={event => console.log('event')}/>
     </div>
   );
 }
