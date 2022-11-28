19dd3ae4adab6daf35c0a3ecdf11c5ee80e6c3cd
Nico Dann <nicodann@gmail.com>
2022-11-28 11:07:52 -0500

    Create User state component.

---
+++ src/components/state/User.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+type AuthUser = {
+  name: string
+  email: string
+}
+
+export const User = () => {
+  const [user,setUser] = useState()
+  const handleLogin = () => {}
+  const handleLogout = () => {}
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is</div>
+    </div>
+  )
+}
